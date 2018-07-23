a7b4e267a8da22d79197b48efb8013a71bad8782
Jit <sarbojit.project@gmail.com>
2018-07-22 18:40:40 -0700

    xxxx

--- config/passport.js
+++ config/passport.js
@@ -12,19 +12,20 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
-        .then(user => {
-          if (user) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
-        .catch(err => console.log('passport error: ' + err));
-      //console.log('jwt_payload regular:' + jwt_payload);
-    })
-  );
+  console.log('opts  keys: ' + JSON.stringify(opts));
+  // passport.use(
+  //   new JwtStrategy(opts, (jwt_payload, done) => {
+  //     User.findById(jwt_payload.id)
+  //       .then(user => {
+  //         if (user) {
+  //           return done(null, user);
+  //         }
+  //         return done(null, false);
+  //       })
+  //       .catch(err => console.log('passport error: ' + err));
+  //     //console.log('jwt_payload regular:' + jwt_payload);
+  //   })
+  // );
 
   passport.use(
     new GoogleStrategy(
